7886c6807b30b3208dfb2d4c8c5d0bc9b7ac1630
Joseph Warren <jswarren@miners.utep.edu>
2019-09-10 18:21:01 -0600

    Changed dictionary from using re.split to re.findall to fix \n being added to dictionary in every line. This also fixed the dashes, (-), between words problem even though I also found a solution when using re.split. Still can't organize alphabetically to save my life

--- wordCount.py
+++ wordCount.py
@@ -13,20 +13,47 @@ if not os.path.exists(textFname):
     print("text file input %s doesn't exist! Exiting" %textFname)
     exit()
 
-outputWriter = open("output.txt", "w+")
-
-wordDictionary = {}
-
+allWords = []
 with open(textFname, 'r') as textFile:
     for line in textFile:
-        for word in re.split("\s", line):
-            word = re.sub("\W", "", word)
-            word = word.lower();
-            if word in wordDictionary:
-                wordDictionary[word]+=1
-            else:
-                wordDictionary[word]=1
-sorted(wordDictionary.items(), key = lambda dictionary: dictionary[0])
+        allWords.extend(re.findall(r"\w+", line))
+        
+wordDictionary = {}
+for word in allWords:
+    word=word.lower()
+    if word in wordDictionary:
+        wordDictionary[word]+=1
+    else:
+        wordDictionary[word]=1
+
+outputWriter = open("output.txt", "w+")
+
+
+
+#alphDict = {}
+#alphList = sorted(wordDictionary.keys())
+#alphDict = {i:0 for i in alphList}
+#for key in alphDict:
+#    alphDict[key]=wordDictionary[key]
+#for word, value in alphDict.items():
+#    outputWriter.write("%s %d\n" % (word, value))
+
+#for key in sorted(wordsFreqDict.keys()):
+#    alphabeticalDictionary[key]
+#    print(key, " :: ", wordsFreqDict[key]
+
+#with open(textFname, 'r') as textFile:
+#    for line in textFile:
+#        print(line)
+#        for word in re.split(r"[-'\s]\s*", line):
+#            print("abc-%s" % word)
+#            word = re.sub("\W", "", word)
+#            word = word.lower();
+#            if word in wordDictionary:
+#                wordDictionary[word]+=1
+#            else:
+#                wordDictionary[word]=1
+#sorted(wordDictionary.items(), key = lambda dictionary: dictionary[0])
 #alphabeticalDictionary = dict.fromkeys(wordDictionary)
 #for word in alphabeticalDictionary:
 #    outputWriter.write("%s %d\n" % (word, alphabeticalDictionary[word]))
@@ -34,5 +61,5 @@ sorted(wordDictionary.items(), key = lambda dictionary: dictionary[0])
 #    print word
 
 
-for word, value in wordDictionary.items():
-    outputWriter.write("%s %d\n" % (word, value))
+#for word, value in wordDictionary.items():
+#    outputWriter.write("%s %d\n" % (word, value))
